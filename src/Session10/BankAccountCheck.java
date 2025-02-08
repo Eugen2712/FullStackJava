e7cd0f128025a13b23d337000cf818c562982906
Eugen2712 <eugeniu.plotnic@gmail.com>
2025-02-08 12:35:35 -0600

    Delete src/Session10 directory

--- src/Session10/BankAccountCheck.java
+++
@@ -1,9 +0,0 @@
-package Session10;
-
-public class BankAccountCheck {
-    public static void main(String[] args) {
-        bankAccount ba = new bankAccount();
-        ba.deposit(500);
-        ba.withdraw(100);
-    }
-}
